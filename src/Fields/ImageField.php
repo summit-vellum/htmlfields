816b392a3d8d60456fddc6a52a681fa10fdf3f69
Jess Castro <gzecastro@gmail.com>
2020-01-23 16:47:50 +0800

    Add Image file upload

--- src/Fields/ImageField.php
+++ src/Fields/ImageField.php
@@ -2,10 +2,22 @@
 
 namespace Quill\Html\Fields;
 
+use Closure;
 use Quill\Html\Fields\BaseField;
-
 
 class ImageField extends BaseField
 {
+    public function disk($disk)
+    {
+        $this->setAttribute('disk', $disk);
 
+        return $this;
+    }
+
+    public function storeAs(Closure $closure)
+    {
+        $this->setAttribute('storeAs', $closure);
+
+        return $this;
+    }
 }
